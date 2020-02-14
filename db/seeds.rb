4652e80ffda8553a74f38f9acaeb075a9095f58d
Laurent Barbezieux <laurent.barbezieux@gmail.com>
2020-02-14 12:00:24 +0100

    new routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mojito = Cocktail.new(name: "mojito")
+mojito.save
+long_island = Cocktail.new(name: "long island")
+long_island.save
+
+lemon = Ingredient.new(name: "lemon")
+lemon.save
+ice = Ingredient.new(name: "ice")
+ice.save
+mint_leaves = Ingredient.new(name: "mint leaves")
+mint_leaves.save
+
